10f73f6d094b0e297932283a46e12db27288fdde
robertp97 <robertp97@interia.pl>
2018-11-08 14:25:55 +0100

    Add project files.

---
+++ GK_P2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace GK_P2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pBox, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(1, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Location = new System.Drawing.Point(637, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(144, 555);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Edycja";
+            // 
+            // pBox
+            // 
+            this.pBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBox.Location = new System.Drawing.Point(3, 3);
+            this.pBox.Name = "pBox";
+            this.pBox.Size = new System.Drawing.Size(628, 555);
+            this.pBox.TabIndex = 1;
+            this.pBox.TabStop = false;
+            this.pBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pBox_MouseDown);
+            this.pBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pBox_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Wypełniacz";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pBox;
+    }
+}
+
---
+++ GK_P2/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GK_P2
+{
+    public partial class Form1 : Form
+    {
+        Point[] vertices = new Point[6];
+        int w, h, r = 5;
+        int chosen_vertex = 0; //indeks wierzchołka, którym chcemy poruszać
+        bool canMove = false;
+        // 0 1 2 - 1szy trójkąt
+        // 3 4 5 - 2gi trójkąt
+        public Form1()
+        {
+            InitializeComponent();
+            pBox.Image = new Bitmap(pBox.Size.Width, pBox.Size.Height);
+            w = pBox.Image.Width;
+            h = pBox.Image.Height;
+            vertices[0] = new Point(20, 20);
+            vertices[1] = new Point(100, 100);
+            vertices[2] = new Point(20, 100);
+            Redraw();
+        }
+
+        private void pBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button==MouseButtons.Left)
+            {
+                if (canMove)
+                    canMove = false;
+                else
+                {
+                    for (int i = 0; i < vertices.Length; ++i)
+                    {
+                        if ((vertices[i].X >= e.X - 2 * r && vertices[i].X <= e.X + 2 * r) && (vertices[i].Y >= e.Y - 2 * r && vertices[i].Y <= e.Y + 2 * r))
+                        {
+                            chosen_vertex = i;
+                            Redraw();
+                            break;
+                        }
+                    }
+                }
+            }
+            if(e.Button==MouseButtons.Right)
+                canMove = true;
+            
+        }
+
+        private void pBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(canMove)
+            {
+                vertices[chosen_vertex] = new Point(e.X, e.Y);
+                Redraw();
+            }
+        }
+
+        private void Redraw()
+        {
+            Image img = new Bitmap(w,h);
+            if (pBox.Image != null)
+                pBox.Image.Dispose();
+            Graphics g = Graphics.FromImage(pBox.Image = img);
+
+            //Krawędzie
+            Pen p = new Pen(Color.Black);
+            for (int i = 0; i < 3; ++i)
+            {
+                g.DrawLine(p, vertices[i], vertices[(i + 1) % 3]);
+            }
+            for (int i = 3; i < 5; ++i)
+            {
+                g.DrawLine(p, vertices[i], vertices[i + 1]);
+            }
+            g.DrawLine(p, vertices[5], vertices[3]);
+
+            //Wierzchłoki
+            foreach (var v in vertices)
+            {
+                g.FillEllipse(new SolidBrush(Color.Black), v.X - r, v.Y - r, 2 * r, 2 * r);
+            }
+            for (int i = 0; i < vertices.Length; ++i)
+            {
+                if (i == chosen_vertex)
+                    g.FillEllipse(new SolidBrush(Color.Red), vertices[i].X - r, vertices[i].Y - r, 2 * r, 2 * r);
+                else
+                    g.FillEllipse(new SolidBrush(Color.Black), vertices[i].X - r, vertices[i].Y - r, 2 * r, 2 * r);
+            }
+        }
+
+        private void Fill()
+        {
+            
+
+        }
+    }
+}
